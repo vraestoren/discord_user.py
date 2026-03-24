4a579affb8416b3d51887f819edd40bcda3e7b35
huok <77536370+vraestoren@users.noreply.github.com>
2026-03-24 14:45:01 +0300

    Update discord_user.py

--- src/discord_user.py
+++ src/discord_user.py
@@ -1,4 +1,3 @@
-from time import time
 from requests import Session
 
 class DiscordUser:
@@ -9,7 +8,22 @@ class DiscordUser:
 			"Content-Type": "application/json",
 			"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.309 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36"
 		}
-		
+
+	def _post(self, endpoint: str, data: dict = None) -> dict:
+		return self.session.post(f"{self.api}{endpoint}", json=data).json()
+
+	def _get(self, endpoint: str, params: dict = None) -> dict:
+		return self.session.get(f"{self.api}{endpoint}", params=params).json()
+
+	def _patch(self, endpoint: str, data: dict = None) -> dict:
+		return self.session.patch(f"{self.api}{endpoint}", json=data).json()
+
+	def _delete(self, endpoint: str) -> dict:
+		return self.session.delete(f"{self.api}{endpoint}").json()
+
+	def _put(self, endpoint: str, data: dict = None) -> dict:
+		return self.session.put(f"{self.api}{endpoint}", json=data).json()
+
 	def login(
 			self,
 			login: str,
@@ -24,58 +38,54 @@ class DiscordUser:
 			"login_source": login_source,
 			"gift_code_sku_id": gift_code_sku_id
 		}
-		response = self.session.post(
-			f"{self.api}/auth/login", json=data).json()
+		response = self._post("/auth/login", data)
 		if "token" in response:
 			self.token = response["token"]
 			self.session.headers["Authorization"] = self.token
 		return response
 
 	def affinities_users(self) -> dict:
-		return self.session.get(
-			f"{self.api}/users/@me/affinities/users").json()
+		return self._get("/users/@me/affinities/users")
 
 	def get_guilds(self) -> dict:
-		return self.session.get(f"{self.api}/users/@me/guilds").json()
+		return self._get("/users/@me/guilds")
 
 	def get_guild_info(self, guild_id: int) -> dict:
-		return self.session.get(f"{self.api}/guilds/{guild_id}").json()
+		return self._get(f"/guilds/{guild_id}")
 
 	def get_guild_channels(self, guild_id: int) -> dict:
-		return self.session.get(
-			f"{self.api}/guilds/{guild_id}/channels").json()
-	
+		return self._get(f"/guilds/{guild_id}/channels")
+
 	def get_account_channels(self) -> dict:
-		return self.session.get(f"{self.api}/users/@me/channels").json()
+		return self._get("/users/@me/channels")
 
 	def leave_guild(self, guild_id: int) -> dict:
-		return self.session.delete(
-            f"{self.api}/users/@me/guilds/{guild_id}").json()
+		return self._delete(f"/users/@me/guilds/{guild_id}")
 
 	def leave_channel(self, channel_id: int) -> dict:
-		return self.session.delete(
-			f"{self.api}/users/@me/channels/{channel_id}").json()
+		return self._delete(f"/users/@me/channels/{channel_id}")
 
 	def discoverable_guilds(
 			self,
 			offset: int = 0,
 			limit: int = 24) -> dict:
-		return self.session.get(
-			f"{self.api}/discoverable-guilds?offset={offset}&limit={limit}").json()
+		params = {
+			"offset": offset,
+			"limit": limit
+		}
+		return self._get("/discoverable-guilds", params)
 
 	def get_relationships(self) -> dict:
-		return self.session.get(f"{self.api}/users/@me/relationships").json()
+		return self._get("/users/@me/relationships")
 
 	def get_user_info(self, user_id: int) -> dict:
-		return self.session.get(f"{self.api}/users/{user_id}/profile").json()
+		return self._get(f"/users/{user_id}/profile")
 
 	def delete_friend(self, user_id: int) -> dict:
-		return self.session.delete(
-			f"{self.api}/users/@me/relationships/{user_id}").json()
+		return self._delete(f"/users/@me/relationships/{user_id}")
 
 	def typing_active(self, channel_id: int) -> dict:
-		return self.session.get(
-			f"{self.api}/channels/{channel_id}/typing").json()
+		return self._post(f"/channels/{channel_id}/typing")
 
 	def change_username(
 			self,
@@ -85,7 +95,7 @@ class DiscordUser:
 			"password": password,
 			"username": username
 		}
-		return self.session.patch(f"{self.api}/users/@me", json=data).json()
+		return self._patch("/users/@me", data)
 
 	def change_status(
 			self,
@@ -97,8 +107,7 @@ class DiscordUser:
 				"text": text
 			}
 		}
-		return self.session.patch(
-			f"{self.api}/users/@me/settings", json=data).json()
+		return self._patch("/users/@me/settings", data)
 
 	def send_message(
 			self,
@@ -107,36 +116,38 @@ class DiscordUser:
 		data = {
 			"content": content
 		}
-		return self.session.post(
-			f"{self.api}/channels/{channel_id}/messages", json=data).json()
+		return self._post(
+            f"/channels/{channel_id}/messages", data)
 
 	def delete_message(
 			self,
 			message_id: int,
 			channel_id: int) -> dict:
-		return self.session.delete(
-			f"{self.api}/channels/{channel_id}/messages/{message_id}").json()
+		return self._delete(
+			f"/channels/{channel_id}/messages/{message_id}")
 
 	def get_channel_messages(
 			self,
 			channel_id: int,
 			limit: int = 100) -> dict:
-		return self.session.get(
-			f"{self.api}/channels/{channel_id}/messages?limit={limit}").json()
+		params = {
+			"limit": limit
+		}
+		return self._get(
+			f"/channels/{channel_id}/messages", params)
 
-	def get_voice_regions(self) -> str:
-		return self.session.get(f"{self.api}/voice/regions").text
+	def get_voice_regions(self) -> dict:
+		return self._get("/voice/regions")
 
 	def get_guild_bans(self, guild_id: int) -> dict:
-		return self.session.get(f"{self.api}/guilds/{guild_id}/bans").json()
+		return self._get(f"/guilds/{guild_id}/bans")
 
 	def get_guild_users(self, guild_id: int) -> dict:
-		return self.session.get(f"{self.api}/guilds/{guild_id}/members").json()
+		return self._get(f"/guilds/{guild_id}/members")
 
 	def delete_channel(self, channel_id: int) -> dict:
-		return self.session.delete(
-			f"{self.api}/channels/{channel_id}").json()
-	
+		return self._delete(f"/channels/{channel_id}")
+
 	def edit_message(
 			self,
 			channel_id: int,
@@ -145,59 +156,53 @@ class DiscordUser:
 		data = {
 			"content": content
 		}
-		return self.session.patch(
-			f"{self.api}/channels/{channel_id}/messages/{message_id}", json=data).json()
+		return self._patch(
+			f"/channels/{channel_id}/messages/{message_id}", data)
 
 	def ban_user_from_guild(
 			self,
 			guild_id: int,
 			user_id: int) -> dict:
-		return self.session.put(
-			f"{self.api}/guilds/{guild_id}/bans/{user_id}").json()
+		return self._put(f"/guilds/{guild_id}/bans/{user_id}")
 
 	def kick_user_from_guild(
 			self,
 			guild_id: int,
 			user_id: int) -> dict:
-		return self.session.delete(
-			f"{self.api}/guilds/{guild_id}/members/{user_id}").json()
+		return self._delete(f"/guilds/{guild_id}/members/{user_id}")
 
 	def create_role(
 			self,
 			name: str,
 			guild_id: int,
-			type: int = 0) -> dict:
+			role_type: int = 0) -> dict:
 		data = {
 			"name": name,
-			"type": type
+			"type": role_type
 		}
-		return self.session.post(
-			f"{self.api}/guilds/{guild_id}/roles", json=data).json()
+		return self._post(f"/guilds/{guild_id}/roles", data)
 
 	def join_guild(self, invite: str) -> dict:
-		return self.session.post(
-			f"{self.api}/invites/{invite}").json()
+		return self._post(f"/invites/{invite}")
 
 	def get_guild_invites(self, guild_id: int) -> dict:
-		return self.session.get(
-			f"{self.api}/guilds/{guild_id}/invites").json()
+		return self._get(f"/guilds/{guild_id}/invites")
 
 	def get_guild_roles(self, guild_id: int) -> dict:
-		return self.session.get(
-			f"{self.api}/guilds/{guild_id}/roles").json()
+		return self._get(f"/guilds/{guild_id}/roles")
 
 	def change_avatar(self, avatar_id: str) -> dict:
 		data = {
 			"avatar": avatar_id
 		}
-		return self.session.patch(f"{self.api}/users/@me", json=data).json()
+		return self._patch(f"/users/@me", data)
 
 	def change_password(
 			self,
 			old_password: str,
 			new_password: str) -> dict:
 		data = {
-			"new_password": new_password,
-			"password": old_password
+			"password": old_password,
+			"new_password": new_password
 		}
-		return self.session.patch(f"{self.api}/users/@me", json=data).json()
+		return self._patch(f"/users/@me", data)
